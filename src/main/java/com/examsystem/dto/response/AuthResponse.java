65933d6210f04d697dc943acf07dd1d589f330da
Topseen-a <abodunrintopseen@gmail.com>
2026-06-02 12:27:06 +0100

    dto setup

---
+++ src/main/java/com/examsystem/dto/response/AuthResponse.java
@@ -0,0 +1,13 @@
+package com.examsystem.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class AuthResponse {
+
+    private String token;
+}
